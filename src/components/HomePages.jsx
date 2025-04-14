357bd757767421ed0ad20b30c0b85fb7417bb302
umang-bit <63403809+umang-bit@users.noreply.github.com>
2025-04-14 10:45:06 +0530

    Update HomePages.jsx

--- src/components/HomePages.jsx
+++ src/components/HomePages.jsx
@@ -1,57 +1,83 @@
 import React, { useState, useEffect } from "react";
 import "./HomePage.css";
-import image1 from "../assets/images/image1.jpg";  
-import image2 from "../assets/images/image2.jpg";  
-import image3 from "../assets/images/image3.jpg";  
+import image1 from "../assets/images/image1.jpg";
+import image2 from "../assets/images/image2.jpg";
+import image3 from "../assets/images/image3.jpg";
 import image4 from "../assets/images/image4.jpg";
-import image5 from "../assets/images/image5.jpg";    
-import HomePageComponent from "../pages/HomePage"; 
-
+import image5 from "../assets/images/image5.jpg";
 
 const HomePage = () => {
-  const images = [image1, image2, image3,image4,image5]; 
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const slides = [
+    {
+      image: image1,
+      title: "Welcome to Rotaract NIT Rourkela",
+      subtitle: "Uniting for a better tomorrow",
+    },
+    {
+      image: image2,
+      title: "Service Above Self",
+      subtitle: "Serving the community with heart and action",
+    },
+    {
+      image: image3,
+      title: "Make the Difference",
+      subtitle: "Be the change you wish to see",
+    },
+    {
+      image: image4,
+      title: "Empower and Inspire",
+      subtitle: "Young leaders transforming lives",
+    },
+    {
+      image: image5,
+      title: "Building a Better World",
+      subtitle: "Through teamwork, service, and leadership",
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 3000); 
-
-    return () => clearInterval(interval); 
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
+    }, 4000);
+    return () => clearInterval(interval);
   }, []);
 
   const handleNext = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
   };
 
   const handlePrevious = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? slides.length - 1 : prevIndex - 1
+    );
   };
+
+  const { image, title, subtitle } = slides[currentIndex];
 
   return (
     <div className="fullscreen-container">
-
       <div className="carousel">
         <button className="arrow left-arrow" onClick={handlePrevious}>
           &#8592;
         </button>
+
         <img
-          src={images[currentImageIndex]} 
-            alt="carousel"
-            className="carousel-image active"
-            style={{ width: "100%", height: "auto", objectFit: "cover" }} 
-            />
-        
+          src={image}
+          alt="slide"
+          className="carousel-image active"
+        />
+
         <div className="carousel-content">
-          <h1>MAKE A DIFFERENCE</h1>
-          <p>Our Mission</p>
+          <h1>{title}</h1>
+          <p>{subtitle}</p>
         </div>
 
         <button className="arrow right-arrow" onClick={handleNext}>
           &#8594;
         </button>
       </div>
-
     </div>
   );
 };
