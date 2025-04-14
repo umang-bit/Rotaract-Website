ce33f44366f14462ca9e66051d6757014c1f218f
umang-bit <63403809+umang-bit@users.noreply.github.com>
2025-04-14 16:40:12 +0530

    Update Title.jsx

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -13,25 +13,9 @@ export default function Title() {
                 <div className='shift-right'>
                 <div className='Event'>
                     <div className='label'>Event
-                    <div className="dropdown">
-                        <select className="dropdown-button" onClick={() => setIsOpen(!isOpen)}>
-                            Event
-                        </select>
-                        {isOpen && (
-                            <div className="dropdown-content">
-                                <a href="#link1">SAPNA</a>
-                                <a href="#link2">Rota-Olympics</a>
-                                <a href="#link3">Down Syndrome Medical Camp</a>
-                            </div>
-                        )}
+                  
                     </div>
-                    </div>
-                    <div className='date'>
-                        <div className='label2'>Date</div>
-                        <div className='date-dropdown'>
-                            <DateDropdown />
-                        </div>
-                    </div>
+
                 </div>
             
             </div>
